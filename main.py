473feaaf635178a7d7b2794789d2e855c22754ea
adkevin3307 <adkevin3307@gmail.com>
2021-04-19 16:51:12 +0800

    add dataset and main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from utils import parse, read_bci_data, show
+from Net import EEGNet, DeepConvNet
+from Model import Model
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    torch.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+
+    args = parse()
+
+    train_loader, test_loader = read_bci_data()
+
+    net = None
+    if args.net == 'EEG':
+        net = EEGNet(args.activation)
+    if args.net == 'DeepConv':
+        net = DeepConvNet(args.activation)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=1e-2)
+
+    model = Model(net, criterion=criterion, optimizer=optimizer)
+    model.summary((1, 1, 2, 750))
+
+    history = model.train(train_loader, epochs=150, val_loader=test_loader)
+    model.test(test_loader)
+
+    show(history)
